72cc61a8be9c8fb28792fdf5770dc0da8dff62b5
eustaquio <eustaquio.galano@gmail.com>
2025-12-05 12:22:04 +0800

    refactor: control input inside components
    - use state variable to control value of input
    - add function to handle every change on input

--- src/components/FormField.jsx
+++ src/components/FormField.jsx
@@ -1,23 +1,25 @@
-function FormField({
-  label = "",
-  type,
-  name,
-  id,
-  placeholder = "",
-  onChangeHandler,
-}) {
+import { useState } from "react";
+
+function FormField({ label = "", type, name, id, placeholder = "" }) {
+  const [value, setValue] = useState("");
+
+  function handleChange(event) {
+    setValue(event.target.value);
+  }
+
   return (
     <div className="form-field">
       <label htmlFor={id} className="form-field__label">
         {label}{" "}
       </label>
       <input
+        value={value}
         type={type}
         name={name}
         id={id}
         placeholder={placeholder}
         className="form-field__input"
-        onChange={onChangeHandler}
+        onChange={handleChange}
       />
     </div>
   );
