812a77c6cdfbf720af6f5f1d244f72b4232d2155
eustaquio <eustaquio.galano@gmail.com>
2025-12-05 17:28:13 +0800

    feat: add EducationalBackgroundContainer component

---
+++ src/components/EducationalBackgroundContainer.jsx
@@ -0,0 +1,18 @@
+import { FormField } from "./FormField";
+
+function EducationalBackgroundContainer() {
+  return (
+    <section className="educational-background">
+      <FormField label="School: " type="text" name="school" id="school" />
+      <FormField
+        label="Date of Study: "
+        type="date"
+        name="date-of-study"
+        id="date-of-study"
+      />
+      <FormField label="Course: " type="text" name="course" id="course" />
+    </section>
+  );
+}
+
+export { EducationalBackgroundContainer };
