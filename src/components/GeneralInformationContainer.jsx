d866bfca35785a8526f5f4ce3d6b28aaf708d143
eustaquio <eustaquio.galano@gmail.com>
2025-12-05 12:00:38 +0800

    feat: add GeneralInformationContainer components
    - Imported and used FormField component as child component

---
+++ src/components/GeneralInformationContainer.jsx
@@ -0,0 +1,18 @@
+import { FormField } from "./FormField";
+
+function GeneralInformationContainer() {
+  return (
+    <section className="general-information">
+      <FormField label="Name: " type="text" name="name" id="name" />
+      <FormField label="Email: " type="email" name="email" id="email" />
+      <FormField
+        label="Contact#: "
+        type="numer"
+        name="contact-number"
+        id="contact-number"
+      />
+    </section>
+  );
+}
+
+export { GeneralInformationContainer };
