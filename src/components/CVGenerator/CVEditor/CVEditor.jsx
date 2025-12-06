7486131e3b96f824917d601eeca0164f08503565
eustaquio <eustaquio.galano@gmail.com>
2025-12-07 01:24:01 +0800

    style: add disable class modifier

--- src/components/CVGenerator/CVEditor/CVEditor.jsx
+++ src/components/CVGenerator/CVEditor/CVEditor.jsx
@@ -5,7 +5,7 @@ import style from "./CVEditor.module.css";
 
 function CVEditor() {
   return (
-    <div className={style["cv-editor"]}>
+    <div className={`${style["cv-editor"]} ${style["cv-editor--disabled"]}`}>
       <GeneralInformationContainer sectionName={style["cv-editor__section"]} />
       <EducationalBackgroundContainer
         sectionName={style["cv-editor__section"]}
