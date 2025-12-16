9ccf518a9ac54b692f3ac0843210a6ab748b2e46
eustaquio <eustaquio.galano@gmail.com>
2025-12-16 13:40:19 +0800

    refactor: move things to CVDisplay component

--- src/components/CVGenerator/CVGenerator.jsx
+++ src/components/CVGenerator/CVGenerator.jsx
@@ -1,25 +1,10 @@
-import { useState } from "react";
 import CVDisplay from "./CVDisplay/CVDisplay";
-import CVEditor from "./CVEditor/CVEditor";
 import styles from "./CVGenerator.module.css";
 
 function CVGenerator() {
-  const [isCVEditorVisible, setIsCVEditorVisible] = useState(false);
-
-  console.log(isCVEditorVisible);
-
-  function editCVVisibility() {
-    if (isCVEditorVisible) {
-      setIsCVEditorVisible(false);
-    } else {
-    setIsCVEditorVisible(true);
-    }
-  }
-
   return (
     <div className={styles["cv-generator"]}>
-      <CVEditor isVisible={isCVEditorVisible} clickHandler={editCVVisibility} />
-      <CVDisplay clickHandler={editCVVisibility} />
+      <CVDisplay />
     </div>
   );
 }
